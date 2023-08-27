2ac2424f0b58472b8634aabeee8fd4822a391e1c
Satish Kumar Sharma <skwebserv@gmail.com>
2023-08-27 12:08:47 +0530

    SK Webs Android Project Created
    This project to create for learning purpose

---
+++ app/src/main/java/com/anshumemorial/skwebs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anshumemorial.skwebs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
